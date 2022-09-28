4f659ffa994bf3592315c359a2600a8edeab9b8e
vladulnik <vladislavmoiseev9@gmail.com>
2022-09-29 00:48:04 +0300

    Add files via upload

---
+++ vlad/vlad/vlad.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+    float c, r, s, pi = 3.14;
+    
+    printf("input r= ");
+    scanf_s("%f", &r);
+
+    c = 2 * pi * r;
+    s = pi * r * r;
+
+    printf("c = %.2f \n", c);
+    printf("s = %.2f", s);
+    return 0;
+
+
+
+
+}
---
+++ vlad2/vlad2/vlad2.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a, b, c;
+	printf("input a = ");
+	scanf_s("%d", &a);
+	printf("input b = ");
+	scanf_s("%d", &b);
+	printf("input c = ");
+	scanf_s("%d", &c);
+
+	if (a >= b && a >= c)
+
+		printf("%d", a);
+
+	else if (b >= a && b >= c)
+
+		printf("%d", b);
+
+	else if (c >= a && c >= b)
+	
+		printf("%d", c);
+
+		return 0;
+}
