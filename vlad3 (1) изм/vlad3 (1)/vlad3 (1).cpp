b49044be1efdc9008abd2e0b865b1dea33013a19
vladulnik <vladislavmoiseev9@gmail.com>
2022-10-07 00:08:35 +0300

    Add files via upload

---
+++ vlad3 (1) изм/vlad3 (1)/vlad3 (1).cpp
@@ -0,0 +1,107 @@
+﻿#include <stdio.h>
+#include <random>
+
+int main()
+{
+    double M[100],c, a = 1;
+    int n, amount = 0, inp = 0;
+    bool boolean = 0;
+  
+    printf("input n = "); //кол.эл.масс.
+    
+    boolean = scanf_s("%d", &n);
+    while (boolean != 1 || n <= 0) 
+    {
+        printf("Pls try again\n");
+        rewind(stdin);
+        boolean = scanf_s("%d", &n);
+    }
+    printf("input c = "); //число с которым надо сравнить
+   
+    boolean = scanf_s("%lf", &c);
+    while (boolean != 1 || c <= 0)
+    {
+        printf("Pls try again\n");
+        rewind(stdin);
+        boolean = scanf_s("%lf", &c);
+    }
+
+    printf("manual input - enter 1 \n"); //выбор ввода 
+    printf("random input - enter 2 \n");
+    
+    boolean = scanf_s("%d", &inp);
+    while (boolean != 1 || inp <= 0 || inp > 2)
+    {
+        printf("Pls try again\n");
+        rewind(stdin);
+        boolean = scanf_s("%d", &inp);
+    }
+    switch (inp)
+    {
+    case 1:
+
+        printf("enter array elements\n");
+        for (int i = 0; i < n; i++)
+        {
+
+            boolean = scanf_s("%lf", &M[i]);
+            while (boolean != 1 || M[i] <= 0)
+            {
+                printf("Pls try again\n");
+                rewind(stdin);
+                boolean = scanf_s("%lf", &M[i]);
+            }
+            printf("\n");
+            if (c <= M[i])
+                amount++;
+        }
+        break;
+
+    case 2:
+        for (int i = 0; i < n; i++)
+        {
+            M[i] = rand() % 100;
+            printf("%lf\n", M[i]);
+            if (c <= M[i])
+                amount++;
+        }
+   
+        break;
+    
+    }
+    
+        int j = 0, max = M[0];
+        for (int i = 0; i < n; i++)
+        {
+            if (M[i] > max)
+            {
+                max = M[i];
+                j = i;
+            }
+      
+        }
+        int y = 0, min = M[0];
+        for (int i = 0; i < n; i++)
+        {
+            if (M[i] < min)
+            {
+                min = M[i];
+                y = i;
+            }
+
+        }
+        if (min <= 0)
+            min = min * (-1);
+        if (min > max)
+            j = y; 
+
+        for (int i = j+1; i < n; i++)
+        {
+            a *= M[i];
+        }
+        printf("multiplication = %lf\n", a);
+
+    
+    printf("amount = %d", amount);
+        return 0;
+}
---
+++ vlad3 (2) изм/vlad3 (2)/vlad3 (2).cpp
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+#include <random>
+
+int main()
+{
+   
+	int M[100],amount = 0, k_element = 0, n = 0, a = 0, inp = 0;
+    bool boolean = 0;
+    printf("input k = ");
+    
+    boolean = scanf_s("%d", &k_element);
+    while (boolean != 1 || k_element <= 0) 
+    {
+        printf("Pls try again\n");
+        rewind(stdin);
+        boolean = scanf_s("%d", &k_element);
+    }
+   
+    printf("input n = ");
+    
+    boolean = scanf_s("%d", &n);
+    while (boolean != 1 || n <= 0) 
+    {
+        printf("Pls try again\n");
+        rewind(stdin);
+        boolean = scanf_s("%d", &n);
+    }
+
+    printf("manual input - enter 1 \n");
+    printf("random input - enter 2 \n");
+
+    boolean = scanf_s("%d", &inp);
+    while (boolean != 1 || inp <= 0 || inp > 2)
+    {
+        printf("Pls try again\n");
+        rewind(stdin);
+        boolean = scanf_s("%d", &inp);
+    }
+
+    switch (inp)
+    {
+    case 1:
+        printf("input array elements: \n");
+        for (int i = 0; i < n; i++)
+        {
+            a++;
+            amount++;
+            boolean = scanf_s("%d", &M[amount]);
+            while (boolean != 1 || M[amount] <= 0)
+            {
+                printf("Pls try again\n");
+                rewind(stdin);
+                boolean = scanf_s("%d", &M[amount]);
+            }
+            if (a % k_element == 0)
+            {
+                amount++;
+                M[amount] = 0;
+            }
+        }
+        break;
+    case 2:
+        for (int i = 0; i < n; i++)
+        {
+            a++;
+            amount++;
+            M[amount] = rand() % 100;
+            printf("%d\n", M[amount]);
+            
+            if (a % k_element == 0)
+            {
+                amount++;
+                M[amount] = 0;
+            }
+ 
+        }
+
+        break;
+    }
+    for (int i = 1; i <= amount; i++)
+        printf("%d ", M[i]);
+
+   
+   
+	return 0;
+}
